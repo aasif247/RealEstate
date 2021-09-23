b3fd25b6874f5f4df27731d7acaa251eb5660ded
aasif247 <47910560+aasif247@users.noreply.github.com>
2021-09-23 17:03:14 +0600

    initial frontend scaffolding done

---
+++ resources/views/frontend/pages/home/home.blade.php
@@ -0,0 +1,20 @@
+@extends('frontend.layouts.app')
+
+@section('title','home')
+
+@push('styles')
+    
+@endpush
+
+@section('content')
+    @include('frontend.pages.home.section.slider')
+    @include('frontend.include.search')
+    @include('frontend.pages.home.section.featured-properties')
+    @include('frontend.pages.home.section.call-to-action')
+    @include('frontend.pages.home.section.testimonials')
+    @include('frontend.pages.home.section.editor')
+@endsection
+
+@push('scripts')
+    
+@endpush
